b79ac8a170d1d627bc5de89e58a1f063c72e9257
Igor0501 <s.igor0501@gmail.com>
2023-07-16 12:44:24 +0300

    pull off

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
+import React, { useState } from 'react';
+
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+import { Container } from './Container/Container';
+
+
+const App = () => {
+
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+
+  const totalFeedbacksCount = () => {
+     const feedbackNumbersArray = Object.values(state);
+     const total = feedbackNumbersArray.reduce(
+       (acc, number) => acc + number,
+       0
+     );
+     return total;
+  };
+
+  const positivePercentageCount = () => {
+    const total = totalFeedbacksCount();
+    if (total) {
+      const positivePercentage = (state.good / total) * 100;
+      return Math.round(positivePercentage);
+    }
+    return '0';
+  };
+
+  const onButtonClick = e => {
+    const value = e.target.textContent;
+    setState(prevValue => ({ ...prevValue, [value]: prevValue[value] + 1}))
+  };
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Container>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={Object.keys(state)}
+            onLeaveFeedback={onButtonClick}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          {totalFeedbacksCount() > 0 ? (
+            <Statistics
+              options={state}
+              total={totalFeedbacksCount}
+              positivePercentage={positivePercentageCount}
+            />
+          ) : (
+            <Notification message={'No feedback given'} />
+          )}
+        </Section>
+      </Container>
+    </>
   );
-};
+}
+
+
+export default App
+
+
+
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Option = styled.p`
+  text-transform: capitalize;
+  font-size: 24px;
+`;
+export const Span = styled.span`
+  list-style: none;
+  // display: flex;
+  gap: 10px;
+  flex-direction: column;
+  margin-left: 40px;
+  font-size: 24px;
+`;
+
+
